9976aabd968bc27951a0756a2cf706a4b430612e
Lentezz <yaroslav.golovan@gmail.com>
2025-08-02 20:09:32 +0300

    Implemented DAO and Service layer for Movie entity

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+
+        Movie movie = new Movie();
+        movie.setDescription("Description");
+        movie.setTitle("Title");
+
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        movie = movieService.add(movie);
+        System.out.println(movie);
+
+        System.out.println(movieService.get(1L));
 
     }
 }
